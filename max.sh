2f8d5d51fb8b7298455b8abadae12eb76173e4a0
Krina Bhalodiya <90252542+Krina2483@users.noreply.github.com>
2022-04-28 01:13:10 +0530

    first commit

---
+++ max.sh
@@ -0,0 +1,23 @@
+echo "Enter 1st num"
+read num1
+echo "Enter 2nd num"
+read num2
+echo "enter 3rd num"
+read num3
+
+if [ $num1 -gt $num2 ]
+then
+    if [ $num1 -gt $num3 ]
+    then
+        echo "$num1 is greater"
+    else
+        echo "$num3 is greater"
+    fi
+else
+    if [ $num2 -gt $num3 ]
+    then
+        echo "$num2 is greater"
+    else
+        echo "$num3 is greater"
+    fi
+fi
